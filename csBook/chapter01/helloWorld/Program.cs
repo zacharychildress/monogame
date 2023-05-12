24ee15cede1f3197baea61f0b4167b3135d946cd
invadr <childresszac@gmail.com>
2023-05-11 19:57:03 -0500

    first program in C#

---
+++ csBook/chapter01/helloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace helloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Commenting works like this in C#
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
